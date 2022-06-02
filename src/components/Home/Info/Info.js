06aa4860d41d6e2cd99406ff62b6487cc5241817
James Suescun <james.steven2318@gmail.com>
2022-06-02 19:30:49 -0400

    refactor Info component

--- src/components/Home/Info/Info.js
+++ src/components/Home/Info/Info.js
@@ -1,38 +1,44 @@
 import styles from "./info.module.css"
+import data from "./data"
 import { Fragment } from "react"
 import { useNavigate } from "react-router-dom"
 
 function Info() {
   let navigate = useNavigate();
-
   const toProjects = () => navigate("/projects")
   const toResume = () => navigate("/resume")
-
+  
   return (
     <Fragment>
         <section className={styles.partTwo}>
           <article>
-            <figure onClick={toProjects} >
-              <img src="https://i.imgur.com/Z6Gw5iT.png"/>
-              <h2>PROJECTS</h2>
-              <p>Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
-              
-            </figure>
-            <figure onClick={toResume}>
-              <img src="https://i.imgur.com/raXHn44.png" />
-              <h2>RESUME</h2>
-              <p>
-              Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc
-              </p>
-              
-            </figure>
-            <figure>
-              <img src="https://i.imgur.com/IpEjczr.png" />
-              <h2>CONTACT</h2>
-              <p>
-              Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laore
-              </p>
-            </figure>
+                <figure onClick={ toProjects }>
+                  {
+                    <Fragment>
+                    <img src={data.projects.img} />
+                    <h2>{data.projects.title}</h2>
+                    <p>{data.projects.description}</p>
+                    </Fragment>
+                  }
+                </figure>
+                <figure onClick={ toResume }>
+                  {
+                    <Fragment>
+                    <img src={data.resume.img} />
+                    <h2>{data.resume.title}</h2>
+                    <p>{data.resume.description}</p>
+                    </Fragment>
+                  }
+                </figure>
+                <figure>
+                  {
+                    <Fragment>
+                    <img src={data.contact.img} />
+                    <h2>{data.contact.title}</h2>
+                    <p>{data.contact.description}</p>
+                    </Fragment>
+                  }
+                </figure>
           </article>
         </section>
     </Fragment>
---
+++ src/components/Home/Info/data.js
@@ -0,0 +1,23 @@
+
+const data = {
+    projects: {
+        img : "https://i.imgur.com/Z6Gw5iT.png",
+        title: "PROJECTS",
+        description: "ras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.",
+        
+    },
+    resume: {
+        img : "https://i.imgur.com/raXHn44.png",
+        title: "RESUME",
+        description: " Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc",
+        
+    },
+    contact: {
+        img : "https://i.imgur.com/IpEjczr.png",
+        title: "CONTACT",
+        description: "Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laore",
+
+    }
+}
+
+export default data
