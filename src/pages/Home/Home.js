3419c113e61428915a28a2ee8c0f3a0a8b79e3f4
James Suescun <james.steven2318@gmail.com>
2022-06-02 12:03:35 -0400

    create About section in Home page & mobile css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,27 @@
+import styles from "./home.module.css"
 import { Fragment } from "react"
 
 function Home() {
   return (
     <Fragment>
-      <section style={{border: "1px solid gray"}}>Home</section>
+      <main className={ styles.homeMain }>
+      <section className={ styles.partOne } >
+        <article style={{border: "1px solid gray"}}>
+          <img src="https://i.imgur.com/nHpbxQO.png"/>
+          <div>
+            <h2> ABOUT </h2>
+            <p>
+              Lorem ipsum dolor sit amet. Quo accusantium ipsum aut neque voluptatum et enim dolores cum obcaecati voluptatem est consequatur quae ut vero galisum et rerum nobis? 33 exercitationem optio eos aliquid pariatur et eveniet expedita eum ullam beatae est dignissimos ipsum qui itaque odio. In commodi iusto eos dolorem dolores nam ducimus sunt eum necessitatibus corrupti. 
+            </p>
+          </div>
+        </article>
+        
+        
+      </section>
+      <section className={styles.partTwo}>
+        
+      </section>
+      </main>
     </Fragment>
   )
 }
